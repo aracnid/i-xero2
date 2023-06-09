5bd6eff604a7c0d0b96375f878616fee9f7cce47
Jason Romano <aracnid@gmail.com>
2023-06-09 16:34:33 -0400

    Build new version

--- i_xero2/__init__.py
+++ i_xero2/__init__.py
@@ -1,10 +1,8 @@
 """A set of functions to retrieve and save data into Xero.
 """
-from importlib.metadata import version
-
 from i_xero2.i_xero import ExpiredCredentialsException
 from i_xero2.i_xero import XeroInterface
 from i_xero2.i_xero_ui import XeroInterfaceUI
 
 
-__version__ = version(__package__)
+__version__ = '2.7.0'
